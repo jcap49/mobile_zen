cfd2465a570c222991eb5c482a7427d8a51bdbd8
jcap49 <jcapecelatro@gmail.com>
2013-09-04 12:19:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MobileZen::Application.config.secret_key_base = '5505ea3bb1796731c20ddda6903df8a650415fd8d264672a122f25a23ce293aca37e3e60e961b91ace725d9d0bff8ccac16a999b05d212796ad2496f94e7e010'
