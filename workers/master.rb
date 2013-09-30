a252f0727bcfca3177c2290dc27a7778c3535b79
jcap49 <jcapecelatro@gmail.com>
2013-09-30 12:16:53 -0400

    more scheduling stuff implemented - still not entirely complete

--- workers/master.rb
+++ workers/master.rb
@@ -1,17 +1,51 @@
+require 'active_record'
 require 'iron_worker_ng'
+require 'pg'
 require 'yaml'
+require 'pry-rails'
+# require '..models/text_message.rb'
 
 # parse settings for iron_worker creds and instantiate client
-config = YAML.load_file("application.yml")
+config = YAML.load_file("../config/application.yml")
 project_id = config['IRONIO_PROJECT_ID']
 token = config['IRONIO_TOKEN']
 iron_worker = IronWorkerNG::Client.new(project_id: project_id, token: token)
 
-TextMessages.each do |text_message|
-  iron_worker.schedules.create("send_sms",
-    text_message_id = text_message.id,
-    {
-      start_at: text_message.send_time.strftime("%I:%M%p") ,
-      run_every: 3600 * 24
-      })
+# parse settings for db creds
+db_config = YAML.load_file("../config/database.yml")
+
+text_message = TextMessage.find_by_id(params['text_message_id']
+
+
+def setup_database
+  puts "Database connection details:#{params['database'].inspect}"
+  return unless params['database']
+  # estabilsh database connection
+  ActiveRecord::Base.establish_connection(params['database'])
 end
+
+def queue_text_message
+  iron_worker.schedules.create("send_sms",
+    {
+      :text_message_id => text_message.id,
+      :start_at => text_message.send_time.strftime("%I:%M%p"),
+      :run_every => 3600 * 24
+    })
+end
+
+# for bulk sending
+# def schedule_bulk_sms
+#   TextMessage.each do |text_message|
+#   iron_worker.schedules.create("send_sms",
+#     {
+#       :database => Rails.configuration.database_configuration[Rails.env],
+#       :text_message_id => text_message.id,
+#       :start_at => text_message.send_time.strftime("%I:%M%p"),
+#       :run_every => 3600 * 24
+#       })
+#   end
+# end
+
+setup_database
+queue_text_message
+# schedule_bulk_sms
--- workers/send_sms.rb
+++ workers/send_sms.rb
@@ -5,16 +5,20 @@ require 'yaml'
 config = YAML.load_file("application.yml")
 account_sid = config['TWILIO_ACCOUNT_SID']
 auth_token = config['TWILIO_AUTH_TOKEN']
-project_id = config['IRONIO_PROJECT_ID']
-token = config['IRONIO_TOKEN']
-text_message = TextMessage.find_by_id(text_message_id)
+
+text_message = TextMessage.find_by_id(params['text_message_id'])
 
 # instantiate twilio client
 twilio_client = Twilio::REST::Client.new(account_sid, auth_token)
 
 # send sms
-twilio_client.account.sms.messages.create(
-  from: TextMessages::TWILIO_PHONE_NUMBER,
-  to: text_message.phone_number,
-  body: text_message.text_body
-  )
+def send_sms
+  twilio_client.account.sms.messages.create(
+    from: TextMessages::TWILIO_PHONE_NUMBER,
+    to: text_message.phone_number,
+    body: text_message.text_body
+  )  
+end
+
+send_sms
+
