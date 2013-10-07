4f69a93d15f9849fdc258e1be7e774fc251f6568
jcap49 <jcapecelatro@gmail.com>
2013-10-07 17:15:09 -0400

    updated creds

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -86,6 +86,6 @@ class TextMessagesController < ApplicationController
     end
 
     def set_iron_client
-      @iron_client = IronWorkerNG::Client.new(project_id: ENV['IRONIO_PROJECT_ID'], token: ENV['IRONIO_TOKEN'])   
+      @iron_client = IronWorkerNG::Client.new(project_id: ENV["IRONIO_PROJECT_ID"], token: ENV["IRONIO_TOKEN"])   
     end
 end
