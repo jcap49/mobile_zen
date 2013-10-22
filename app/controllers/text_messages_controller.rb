b61e4c3ee56bd755a1888a80a00bcf89303f0d1f
jcap49 <jcapecelatro@gmail.com>
2013-10-21 20:26:30 -0400

    refactored text message controller some more - yahoo

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,18 +1,5 @@
 class TextMessagesController < ApplicationController
   before_action :set_text_message, only: [:show, :edit, :update, :destroy]
-  # before_action :set_twilio_client, only: [:send_welcome_text_message]
-  
-  # def index
-  #   @text_messages = TextMessage.all
-  # end
-
-  # def show
-  # end
-
-  # def edit
-  # end
-
-
 
   def create  
     @text_message = TextMessage.new(text_message_params)
@@ -24,21 +11,10 @@ class TextMessagesController < ApplicationController
     if @text_message.save
       session[:text_message_id] = @text_message.id
       redirect_to new_user_registration_path
-      # send_welcome_text_message(text_message_params[:phone_number])
-      # execute_text_message_worker(@text_message.id, @text_message.send_time)
     else
       redirect_to root_path, notice: "Whoops something went wrong - give it another go."
     end
   end
-
-  # def update    
-  #     if @text_message.update(text_message_params)
-  #       redirect_to @text_message, notice: 'Text message was successfully updated.' 
-  #     else
-  #       render action: 'edit' 
-  #     end
-  #   end
-  # end
 
   def destroy(phone_number)
     text_message = TextMessage.find_by_phone_number(phone_number)
@@ -63,51 +39,6 @@ class TextMessagesController < ApplicationController
       params.require(:text_message).permit(:phone_number, :text_body, :send_time, :user_id)
     end
 
-    def set_twilio_client
-      @twilio_client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
-    end
-
-    # commented these two methods out as they've been
-    # moved to the custom_users controller;
-    # will delete once i've sorted out the refactoring
-    # completely
-
-    # def send_welcome_text_message(phone_number)
-    #   set_twilio_client
-    #   if TextMessage.find_by_phone_number(phone_number) == nil
-    #     @twilio_client.account.sms.messages.create(
-    #       from: TextMessage::TWILIO_PHONE_NUMBER,
-    #       to: phone_number,
-    #       body: TextMessage::UNREGISTERED_WELCOME 
-    #       )
-    #   elsif TextMessage.find_by_phone_number(phone_number) != nil
-    #     @twilio_client.account.sms.messages.create(
-    #       from: TextMessage::TWILIO_PHONE_NUMBER,
-    #       to: phone_number,
-    #       body: TextMessage::REGISTERED_WELCOME
-    #       )
-    #   end
-    # end
-
-    # def execute_text_message_worker(text_message_id, send_time)
-    #   iron_worker = IronWorkerNG::Client.new
-    #   iron_worker.schedules.create("Master", { 
-    #       :text_message_id => text_message_id,
-    #       :start_at => send_time,
-    #       :run_every => 3600 * 24,
-    #       :run_times => 365,
-    #       :database => Rails.configuration.database_configuration[Rails.env]
-    #     })
-    # end
-
-    def update_registration(phone_number)
-      text_message = TextMessage.find_by_phone_number(phone_number)
-      user_id = text_message.user_id
-      user = User.find_by_id(user_id)
-      user.update_attribute("registered", true)
-      user.save!
-    end
-
     def parse_text_message_body(text_message_body, phone_number)
       if text_message_body.downcase == 'yes' 
         update_registration(phone_number)
@@ -116,5 +47,13 @@ class TextMessagesController < ApplicationController
         destroy(phone_number)
         render 'unsubscribe.xml.erb', content_type: 'text/xml'
       end   
+    end
+
+    def update_registration(phone_number)
+      text_message = TextMessage.find_by_phone_number(phone_number)
+      user_id = text_message.user_id
+      user = User.find_by_id(user_id)
+      user.update_attribute("registered", true)
+      user.save!
     end
 end
