09fa12148dfb92e7e32813bcabc0d67b27e31512
jcap49 <jcapecelatro@gmail.com>
2013-10-29 09:51:17 -0400

    removed binding.pry

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -23,8 +23,7 @@ class TextMessagesController < ApplicationController
       @text_message.user_id = current_user.id
       @text_message.save
       sanitize_phone_number(@text_message)
-      # send_registered_welcome_text_message(@text_message.phone_number)
-      binding.pry
+      send_registered_welcome_text_message(@text_message.phone_number)
       TextMessage.execute_text_message_worker(@text_message.id, @text_message.send_time, @text_message.user_id)
       redirect_to root_path, notice: "Great - you're all sorted."
     elsif @text_message.save 
